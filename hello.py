466970857b88a2775e375ece0c8d1e08cc0317e1
Jonathan Martin <44706383+jonbmartin@users.noreply.github.com>
2020-06-24 12:48:50 -0500

    Add numpy functions

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
+import numpy
 print("Hello from Jonathan's first Binder!")
+print("You're running a custom python environment I set up - in your browser!")
+print("This environment has: ")
+print(numpy.__version__)
+print("Let's use numpy to get a random number! How about:")
+print(numpy.random.randn())
+
+
