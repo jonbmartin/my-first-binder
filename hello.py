4fc371a6e9e09e7a1362a1a42f8fe495ecb5de31
Jonathan Martin <44706383+jonbmartin@users.noreply.github.com>
2020-06-24 12:50:20 -0500

    Update text

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 import numpy
 print("Hello from Jonathan's first Binder!")
 print("You're running a custom python environment I set up - in your browser!")
-print("This environment has: ")
+print("This environment has numpy version: ")
 print(numpy.__version__)
 print("Let's use numpy to get a random number! How about:")
 print(numpy.random.randn())
